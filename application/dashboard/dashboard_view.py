52b65b783b42abc2bf13557853e1b830e9e8cb58
Patrikas <219868151+patrikas-git@users.noreply.github.com>
2025-07-21 11:02:46 +0200

    Dashboards

--- application/dashboard/dashboard_view.py
+++ application/dashboard/dashboard_view.py
@@ -1,12 +1,80 @@
-from flask import (Blueprint,
-					render_template
-					)
-from .. import get_userinfo
+from flask import Blueprint, jsonify, render_template
+from database.database import Database
+import json
+import os
+import pandas as pd
+import time
+
+dashboard_blueprint = Blueprint(
+    "dashboard",
+    __name__,
+    url_prefix="/dashboard",
+    template_folder="templates",
+    static_folder="static",
+)
 
 
-dashboard_blueprint = Blueprint('dashboard', __name__, url_prefix='/dashboard', template_folder='templates', static_folder='static')
+OUTPUT_FILE = os.path.join("/tmp", "workflow_transitions.json")
 
-@dashboard_blueprint.route('', strict_slashes=False, methods=['GET'])
+
+@dashboard_blueprint.route("", strict_slashes=False, methods=["GET"])
 def show_dashboard():
-	user = get_userinfo()
-	return render_template('Dashboard.html.jinja')
+    return render_template("Dashboard.html.jinja")
+
+
+@dashboard_blueprint.route("/dashboard-data", methods=["GET"])
+def get_dashboard_data():
+    return get_histograms()
+
+
+def get_histograms() -> dict:
+    start_time = time.time()
+    relval_db = Database("relvals")
+    relvals_collection = relval_db.collection
+
+    query = {"workflows": {"$ne": []}}
+    projection = {"_id": 0, "workflows": 1}
+
+    all_events = []
+    for doc in relvals_collection.find(query, projection):
+        for workflow in doc.get("workflows", []):
+            workflow_name = workflow.get("name")
+            for history_event in workflow.get("status_history", []):
+                all_events.append(
+                    {
+                        "workflow_name": workflow_name,
+                        "status": history_event.get("status"),
+                        "time": history_event.get("time"),
+                    }
+                )
+
+    if not all_events:
+        print("No events found to process.")
+        return
+
+    df = pd.DataFrame(all_events)
+    transitions = []
+    for _, group in df.groupby("workflow_name"):
+        group_transitions = pd.DataFrame(
+            {
+                "from": group["status"],
+                "to": group["status"].shift(-1),
+                "start": group["time"],
+                "duration_seconds": group["time"].shift(-1) - group["time"],
+            }
+        )
+
+        group_transitions.dropna(inplace=True)
+        group_transitions["duration_seconds"] = group_transitions[
+            "duration_seconds"
+        ].astype(int)
+        transitions.extend(group_transitions.to_dict("records"))
+
+    print(f"Writing {len(transitions)} transitions to {OUTPUT_FILE}")
+    with open(OUTPUT_FILE, "w") as f:
+        json.dump(transitions, f)
+
+    print("Worker finished successfully.")
+    print("--- %s seconds ---" % (time.time() - start_time))
+
+    return jsonify(transitions)
--- application/dashboard/static/js/dashboard.js
+++ application/dashboard/static/js/dashboard.js
@@ -1,57 +1,57 @@
-function fetchWorkerInfo(){
-  fetch('api/system/workers').then(res => res.json()).then(d =>{
+function fetchWorkerInfo() {
+  fetch('api/system/workers').then(res => res.json()).then(d => {
     submissionWorkers = d.response;
-    $('#threads').html('Submission threads ('+Object.keys(submissionWorkers).length+')')
+    $('#threads').html('Submission threads (' + Object.keys(submissionWorkers).length + ')')
     $('#threads-list').html("")
     for (var i in submissionWorkers) {
       let job_name = submissionWorkers[i].job_name;
       let job_time = submissionWorkers[i].job_time;
       var info = job_name ? 'working on ' + job_name + ' for ' + job_time + 's' : 'not busy'
-      $('#threads-list').append('<li>Thread '+i+' is '+info+'</li>')
+      $('#threads-list').append('<li>Thread ' + i + ' is ' + info + '</li>')
     }
   })
 }
 
 function fetchQueueInfo() {
-  fetch('api/system/queue').then(res => res.json()).then(d =>{
+  fetch('api/system/queue').then(res => res.json()).then(d => {
     submissionQueue = d.response;
-    $('#queue').html('Submission queue ('+submissionQueue.length+')')
+    $('#queue').html('Submission queue (' + submissionQueue.length + ')')
     $('#queue-list').html("")
     for (var i = 0; i < submissionQueue.length; i++) {
-      $('#queue-list').append('<li><a href="relvals?prepid="'+submissionQueue[i]+' title="Show this RelVal"></a> is waiting in queue</li>')
+      $('#queue-list').append('<li><a href="relvals?prepid="' + submissionQueue[i] + ' title="Show this RelVal"></a> is waiting in queue</li>')
     }
   })
 }
 
 function fetchLocksInfo() {
-  fetch('api/system/locks').then(res => res.json()).then(d =>{
+  fetch('api/system/locks').then(res => res.json()).then(d => {
     locks = d.response;
-    $('#locks').html('Locked objects  ('+Object.keys(locks).length+')')
+    $('#locks').html('Locked objects  (' + Object.keys(locks).length + ')')
     $('#locks-list').html("")
     for (var i in locks) {
-      let style=locks[i] ? "color: red; font-weight: bold;" : "";
-      $('#locks-list').append('<li style="'+style+'"> '+locks[i]+': '+i+'</li>')
+      let style = locks[i] ? "color: red; font-weight: bold;" : "";
+      $('#locks-list').append('<li style="' + style + '"> ' + locks[i] + ': ' + i + '</li>')
     }
   })
 }
 
 function fetchSettings() {
-  fetch('api/settings/get').then(res => res.json()).then(d =>{
+  fetch('api/settings/get').then(res => res.json()).then(d => {
     settings = d.response;
   })
 }
 
 function fetchUptime() {
-  fetch('api/system/uptime').then(res => res.json()).then(d =>{
+  fetch('api/system/uptime').then(res => res.json()).then(d => {
     uptime = d.response;
-    let uptime_content = uptime.days+' days '+uptime.hours+' hours '+uptime.minutes+' minutes '+uptime.seconds+' seconds'
-    $('#system-uptime-list').html('<li>'+uptime_content+'</li>');
-  }) 
+    let uptime_content = uptime.days + ' days ' + uptime.hours + ' hours ' + uptime.minutes + ' minutes ' + uptime.seconds + ' seconds'
+    $('#system-uptime-list').html('<li>' + uptime_content + '</li>');
+  })
 }
 
 function fetchBuildInfo() {
-  fetch('api/system/build_info').then(res => res.json()).then(d =>{
+  fetch('api/system/build_info').then(res => res.json()).then(d => {
     buildInfo = d.response;
-    $('#build-info-list').html('<li>Build version: '+buildInfo+'</li>')
+    $('#build-info-list').html('<li>Build version: ' + buildInfo + '</li>')
   })
 }
